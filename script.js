e2d13c0e59447de5386579f9799672e8309755eb
Ajay-Parmar111 <ajayparemar1817@gmail.com>
2024-07-17 18:33:28 +0530

    created business name generator

---
+++ script.js
@@ -0,0 +1,32 @@
+let rand = Math.random();
+let first, second, third;
+
+// first word
+if (rand < 0.33) {
+  first = "Crazy";
+} else if (rand < 0.66 && rand >= 0.33) {
+  first = "Amazing";
+} else {
+  first = "Fire";
+}
+
+// second word
+if (rand < 0.33) {
+  second = "Engine";
+} else if (rand < 0.66 && rand >= 0.33) {
+  second = "Foods";
+} else {
+  second = "Garments";
+}
+
+// third word
+if (rand < 0.33) {
+  third = "Bros";
+} else if (rand < 0.66 && rand >= 0.33) {
+  third = "Limited";
+} else {
+  third = "Hub";
+}
+
+// result
+alert(`result is ${first} ${second} ${third}`)
